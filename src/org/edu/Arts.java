d207bf39eaf6a10f3201ec190d4e1df6edd4ed69
jothidharman <jothi.dharman95@gmail.com>
2022-10-18 15:20:32 +0530

    New Project 2

---
+++ src/org/edu/Arts.java
@@ -0,0 +1,53 @@
+package org.edu;
+
+//child class
+
+public class Arts extends Education {
+
+	@Override
+	public void bSc() {
+		System.out.println("Course---------->Bsc");
+		
+	}
+
+	@Override
+	public void bEd() {
+		System.out.println("Course---------->B.Ed");
+		
+	}
+
+	@Override
+	public void bA() {
+		System.out.println("Course---------->BA");
+		
+	}
+
+	@Override
+	public void bBA() {
+		System.out.println("Course---------->BBA");
+		
+	}
+
+	@Override
+	public void ug() {
+		System.out.println("UG---------->Bsc \n B.Ed \n BA \n BBA");
+		
+	}
+
+	@Override
+	public void pg() {
+		System.out.println("PG---------->MBA \n M.com \n M.E");
+		
+	}
+	
+	public static void main(String[] args) {
+		Arts as = new Arts();
+		as.bSc();
+		as.bEd();
+		as.bA();
+		as.bBA();
+		as.ug();
+		as.pg();
+	}
+
+}
---
+++ src/org/edu/Education.java
@@ -0,0 +1,11 @@
+package org.edu;
+//parent class
+public abstract class Education {
+	
+	public abstract void bSc();
+	public abstract void bEd();
+	public abstract void bA();
+	public abstract void bBA();
+	public abstract void ug();
+	public abstract void pg();
+}
